2061ad544783c3ec932b73bf0b88d3e7399d57ab
Tuva Aarseth <tuvaea@uia.no>
2025-01-29 17:20:13 +0100

    Update url

--- swyve-frontend/src/Profile.js
+++ swyve-frontend/src/Profile.js
@@ -5,7 +5,7 @@ import './Profile.css';
 function Profile() {
   const [streak, setStreak] = useState(0);
   const [playlists, setPlaylists] = useState([]);
-  const BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://localhost:5000"; // Backend-URL
+  const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:5000"; // Backend-URL
 
   const user = {
     username: 'User123',
