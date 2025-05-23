3e5e5ed1627138ec852d57d2fc2ef631a6eb90e8
Ari-rizz <arian.rizzuti@gokstadakademiet.no>
2025-05-23 15:02:45 +0200

    test

--- swyve-frontend/src/services/passwordService.js
+++ swyve-frontend/src/services/passwordService.js
@@ -1,4 +1,4 @@
-const API_URL = process.env.REACT_APP_BASE_URL; 
+const API_URL = process.env.REACT_APP_API_URL;
 
 export async function sendResetEmail(email) {
   const res = await fetch(`${API_URL}/forgot-password`, {
