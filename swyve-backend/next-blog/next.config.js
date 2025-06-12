d4a35511466d70cbfa536d163ded3829b7b34cb7
Ari-rizz <arian.rizzuti@gokstadakademiet.no>
2025-06-12 12:36:38 +0200

    config 2

--- swyve-backend/next-blog/next.config.js
+++ swyve-backend/next-blog/next.config.js
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: '/blog',
-  assetPrefix: '/blog',
-  trailingSlash: true,
-  output: 'export',
   images: {
     remotePatterns: [
       {
@@ -20,7 +16,14 @@ const nextConfig = {
   env: {
     NEXT_PUBLIC_WP_API: process.env.NEXT_PUBLIC_WP_API,
   },
+  async rewrites() {
+    return [
+      {
+        source: '/blog/:path*',
+        destination: 'https://swyve-backend.onrender.com/blog/:path*',
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
-
